10788052f4973cb69d61f21ecb0fd589d57a0694
aflores <aflores@cosimti.com>
2020-02-01 22:03:25 -0400

    First

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherLocation></WeatherLocation>
+    </div>
+  );
+}
+
+export default App;
